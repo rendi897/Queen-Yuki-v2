9741171ec5168d103295aa9ecb3265f269ac14fe
rendi897 <84292422+rendi897@users.noreply.github.com>
2024-10-03 17:41:30 +0700

    Add files via upload

---
+++ command.js
@@ -0,0 +1,114 @@
+require("./config.js")
+const fs = require("fs")
+
+const {
+    getGroupAdmins,
+} = require("./lib/library.js")
+
+module.exports = async (fell, m) => {
+    try {
+        const body = (
+            (m.mtype === 'conversation' && m.message.conversation) ||
+            (m.mtype === 'imageMessage' && m.message.imageMessage.caption) ||
+            (m.mtype === 'documentMessage' && m.message.documentMessage.caption) ||
+            (m.mtype === 'videoMessage' && m.message.videoMessage.caption) ||
+            (m.mtype === 'extendedTextMessage' && m.message.extendedTextMessage.text) ||
+            (m.mtype === 'buttonsResponseMessage' && m.message.buttonsResponseMessage.selectedButtonId) ||
+            (m.mtype === 'templateButtonReplyMessage' && m.message.templateButtonReplyMessage.selectedId)
+        ) ? (
+            (m.mtype === 'conversation' && m.message.conversation) ||
+            (m.mtype === 'imageMessage' && m.message.imageMessage.caption) ||
+            (m.mtype === 'documentMessage' && m.message.documentMessage.caption) ||
+            (m.mtype === 'videoMessage' && m.message.videoMessage.caption) ||
+            (m.mtype === 'extendedTextMessage' && m.message.extendedTextMessage.text) ||
+            (m.mtype === 'buttonsResponseMessage' && m.message.buttonsResponseMessage.selectedButtonId) ||
+            (m.mtype === 'templateButtonReplyMessage' && m.message.templateButtonReplyMessage.selectedId)
+        ) : '';
+
+        const budy = (typeof m.text === 'string') ? m.text : '';
+        const prefixRegex = /^[°zZ#$@*+,.?=''():√%!¢£¥€π¤ΠΦ_&><`™©®Δ^βα~¦|/\\©^]/;
+        const prefix = prefixRegex.test(body) ? body.match(prefixRegex)[0] : '.';
+        const isCmd = body.startsWith(prefix);
+        const command = isCmd ? body.slice(prefix.length).trim().split(' ').shift().toLowerCase() : '';
+        const args = body.trim().split(/ +/).slice(1)
+        const text = q = args.join(" ")
+        const sender = m.key.fromMe ? (fell.user.id.split(':')[0] + '@s.whatsapp.net' || fell.user.id) : (m.key.participant || m.key.remoteJid)
+        const botNumber = await fell.decodeJid(fell.user.id)
+        const senderNumber = sender.split('@')[0]
+        const pushname = m.pushName || `${senderNumber}`
+        const isBot = botNumber.includes(senderNumber)
+        const fatkuns = (m.quoted || m)
+        const quoted = (fatkuns.mtype == 'buttonsMessage') ? fatkuns[Object.keys(fatkuns)[1]] : (fatkuns.mtype == 'templateMessage') ? fatkuns.hydratedTemplate[Object.keys(fatkuns.hydratedTemplate)[1]] : (fatkuns.mtype == 'product') ? fatkuns[Object.keys(fatkuns)[0]] : m.quoted ? m.quoted : m
+        const mime = (quoted.m || quoted).mimetype || ''
+        const qmsg = (quoted.m || quoted)
+        const isCreator = (m && m.sender && [botNumber, ...global.owner].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)) || false;
+
+        const groupMetadata = m.isGroup ? await fell.groupMetadata(m.chat).catch(e => { }) : ''
+        const groupName = m.isGroup ? groupMetadata.subject : ''
+        const participants = m.isGroup ? await groupMetadata.participants : ''
+        const groupAdmins = m.isGroup ? await getGroupAdmins(participants) : ''
+        const isBotAdmins = m.isGroup ? groupAdmins.includes(botNumber) : false
+        const isAdmins = m.isGroup ? groupAdmins.includes(m.sender) : false
+        const groupOwner = m.isGroup ? groupMetadata.owner : ''
+        const isGroupOwner = m.isGroup ? (groupOwner ? groupOwner : groupAdmins).includes(m.sender) : false
+
+        if (isCmd) console.log("~> [CMD]", command, "from", pushname, "in", m.isGroup ? "Group Chat" : "Private Chat", '[' + args.length + ']');
+
+        switch (command) {
+            case 'menu':
+                m.reply('Hola')
+                break;
+
+            default:
+                if (budy.startsWith('=>')) {
+                    if (!isCreator) return
+                    function Return(sul) {
+                        sat = JSON.stringify(sul, null, 2)
+                        bang = require('util').format(sat)
+                        if (sat == undefined) {
+                            bang = require('util').format(sul)
+                        }
+                        return m.reply(bang)
+                    }
+                    try {
+                        m.reply(require('util').format(eval(`(async () => { return ${budy.slice(3)} })()`)))
+                    } catch (e) {
+                        m.reply(String(e))
+                    }
+                }
+
+                if (budy.startsWith('>')) {
+                    if (!isCreator) return
+                    let kode = budy.trim().split(/ +/)[1]
+                    let teks
+                    try {
+                        teks = /await/i.test(kode) ? eval("(async() => { " + kode + " })()") : eval(kode)
+                    } catch (e) {
+                        teks = e
+                    } finally {
+                        await m.reply(require('util').format(teks))
+                    }
+                }
+
+                if (budy.startsWith('$')) {
+                    if (!isCreator) return
+                    exec(budy.slice(2), (err, stdout) => {
+                        if (err) return m.reply(`${err}`)
+                        if (stdout) return m.reply(stdout)
+                    })
+                }
+                break;
+                break;
+        }
+    } catch (err) {
+        console.log(require('util').format(err));
+    }
+};
+
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+    fs.unwatchFile(file);
+    console.log(`Update ${__filename}`);
+    delete require.cache[file];
+    require(file);
+});
---
+++ config.js
@@ -0,0 +1,27 @@
+const fs = require("fs")
+
+global.owner = ["6283850246271", "6200000000000", "621111111111"];
+
+//watermark 
+global.packname = 'Author'
+global.author = 'RenzGG'
+global.idcennel = '<id_saluran>@newsletter'
+global.thumb = 'https://imgur.com/a/aTJIgvn' // Your Thumbnail
+
+global.mess = {
+    success: 'Done Abangku 🔥',
+    admin: '_*❗Perintah Ini Hanya Bisa Digunakan Oleh Admin Group !*_',
+    botAdmin: '_*❗Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin Group !*_',
+    owner: '_*❗Perintah Ini Hanya Bisa Digunakan Oleh Owner !*_',
+    group: '_*❗Perintah Ini Hanya Bisa Digunakan Di Group Chat !*_',
+    private: '_(❗Perintah Ini Hanya Bisa Digunakan Di Private Chat !*_',
+    wait: '_*⏳ Sedang Di Proses !*_',
+}
+
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+    fs.unwatchFile(file);
+    console.log(`Update ${__filename}`);
+    delete require.cache[file];
+    require(file);
+});
